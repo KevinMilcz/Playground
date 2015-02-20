0106fc6bf3bdd7db278e4c28545623d0bc040450
KevinMilcz <kmilcze1@binghamton.edu>
2015-02-20 00:30:33 -0500

    Switch the demo one to use array subcripts for findNext

--- pf_clean.c
+++ pf_clean.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/* define a type for a function that takes a pointer to a void pointer as
+/* define a type for a function that takes an array of void pointers as
  * as an argument, and returns a void pointer */
-typedef void* (*func)(void**);
+typedef void* (*func)(void* arg[]);
 
 /* pointer to an array of function pointers with type func */
 func (*a)[10];
 
-/* Input is a pointer to a function pointer, presumably with some accessible memory
- * after it (an array perhaps?). This function simply copies that function pointer, 
- * to the next spot in the array, and prints out the index offset from the global a */
-void* findNext(void** arg)
+/* Input is an array of void pointers, which are actually function pointers but we can't
+ * really specify a function as taking an argument with a funtion pointer with the same signature.
+ * (it goes recursive)
+ * This function simply copies the function pointer in index 0 to index 1, and 
+ * prints out the index offset from the global a */
+void* findNext(void* arg[])
 {
-	void**next = (void**)((uintptr_t)arg + sizeof(arg));
-	*next = *arg;
-	printf("index: %d\n", (int)( ((uintptr_t)next - (uintptr_t)*a) / sizeof(arg) -1) );
-	return *next;	
+	arg[1] = arg[0];
+	printf("index: %d\n", (int)( ((uintptr_t)&arg[1] - (uintptr_t)*a) / sizeof(*arg) -1) );
+	return arg[1];	
 }
 
 int main(int argc, char**argv)
@@ -34,7 +35,7 @@ int main(int argc, char**argv)
 	 * pray that someone else filled in the next one before we call it... */
 	for(i = 0; i < sizeof(*a)/sizeof(**a) -1; i++)
 	{	
-		(*a)[i]( (void*)&(*a)[i] );
+		(*a)[i]( (void**)&(*a)[i] );
 	}
 	
 	/* Just to shut off unused param warning*/
